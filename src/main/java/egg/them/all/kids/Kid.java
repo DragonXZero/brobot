e726122c6457ec616e8082d8c8081ed3ea2b1121
xlunardargoonx <phillipho1993@gmail.com>
2019-05-02 23:25:06 -0700

    Combine multiple lines comment into one line comment.

--- src/main/java/egg/them/all/kids/Kid.java
+++ src/main/java/egg/them/all/kids/Kid.java
@@ -1,8 +1,6 @@
 package egg.them.all.kids;
 
-/* This is a temp Kid class
- * Other functions will be added later
- * when we hash out the kid.
+/* This is a temp Kid class. Other functions will be added later when we hash out the kid.
  */
 public abstract class Kid
 {
